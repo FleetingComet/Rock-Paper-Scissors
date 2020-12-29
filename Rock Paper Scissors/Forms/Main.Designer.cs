ab7d5ab037c4440e2a2e78c43db95af58c3624cf
Artemis of the Blue <alfred_paguio@yahoo.com>
2020-12-29 13:59:54 +0800

    upload to github
    no image yet

---
+++ Rock Paper Scissors/Forms/Main.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Rock_Paper_Scissors
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRock = new System.Windows.Forms.Button();
+            this.btnPaper = new System.Windows.Forms.Button();
+            this.btnScissors = new System.Windows.Forms.Button();
+            this.userScoreLabel = new System.Windows.Forms.Label();
+            this.computerScoreLabel = new System.Windows.Forms.Label();
+            this.userPictureBox = new System.Windows.Forms.PictureBox();
+            this.computerPictureBox = new System.Windows.Forms.PictureBox();
+            this.statusLabel = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnRestart = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.userPictureBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnRock
+            // 
+            this.btnRock.BackColor = System.Drawing.Color.Brown;
+            this.btnRock.FlatAppearance.BorderSize = 0;
+            this.btnRock.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRock.Location = new System.Drawing.Point(124, 289);
+            this.btnRock.Name = "btnRock";
+            this.btnRock.Size = new System.Drawing.Size(76, 76);
+            this.btnRock.TabIndex = 0;
+            this.btnRock.Text = "Rock";
+            this.btnRock.UseVisualStyleBackColor = false;
+            this.btnRock.Click += new System.EventHandler(this.buttonPress);
+            // 
+            // btnPaper
+            // 
+            this.btnPaper.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.btnPaper.FlatAppearance.BorderSize = 0;
+            this.btnPaper.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnPaper.ForeColor = System.Drawing.Color.Black;
+            this.btnPaper.Location = new System.Drawing.Point(271, 289);
+            this.btnPaper.Name = "btnPaper";
+            this.btnPaper.Size = new System.Drawing.Size(76, 76);
+            this.btnPaper.TabIndex = 1;
+            this.btnPaper.Text = "Paper";
+            this.btnPaper.UseVisualStyleBackColor = false;
+            this.btnPaper.Click += new System.EventHandler(this.buttonPress);
+            // 
+            // btnScissors
+            // 
+            this.btnScissors.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnScissors.FlatAppearance.BorderSize = 0;
+            this.btnScissors.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnScissors.Location = new System.Drawing.Point(416, 289);
+            this.btnScissors.Name = "btnScissors";
+            this.btnScissors.Size = new System.Drawing.Size(76, 76);
+            this.btnScissors.TabIndex = 2;
+            this.btnScissors.Text = "Scissors";
+            this.btnScissors.UseVisualStyleBackColor = false;
+            this.btnScissors.Click += new System.EventHandler(this.buttonPress);
+            // 
+            // userScoreLabel
+            // 
+            this.userScoreLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.userScoreLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.userScoreLabel.Location = new System.Drawing.Point(44, 27);
+            this.userScoreLabel.Name = "userScoreLabel";
+            this.userScoreLabel.Size = new System.Drawing.Size(209, 29);
+            this.userScoreLabel.TabIndex = 3;
+            this.userScoreLabel.Text = "User: 0";
+            // 
+            // computerScoreLabel
+            // 
+            this.computerScoreLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.computerScoreLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.computerScoreLabel.Location = new System.Drawing.Point(354, 27);
+            this.computerScoreLabel.Name = "computerScoreLabel";
+            this.computerScoreLabel.Size = new System.Drawing.Size(209, 29);
+            this.computerScoreLabel.TabIndex = 4;
+            this.computerScoreLabel.Text = "Computer: 0";
+            // 
+            // userPictureBox
+            // 
+            this.userPictureBox.BackColor = System.Drawing.Color.DimGray;
+            this.userPictureBox.Location = new System.Drawing.Point(48, 79);
+            this.userPictureBox.Name = "userPictureBox";
+            this.userPictureBox.Size = new System.Drawing.Size(205, 153);
+            this.userPictureBox.TabIndex = 5;
+            this.userPictureBox.TabStop = false;
+            // 
+            // computerPictureBox
+            // 
+            this.computerPictureBox.BackColor = System.Drawing.Color.DimGray;
+            this.computerPictureBox.Location = new System.Drawing.Point(358, 79);
+            this.computerPictureBox.Name = "computerPictureBox";
+            this.computerPictureBox.Size = new System.Drawing.Size(205, 153);
+            this.computerPictureBox.TabIndex = 6;
+            this.computerPictureBox.TabStop = false;
+            // 
+            // statusLabel
+            // 
+            this.statusLabel.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.statusLabel.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.statusLabel.Location = new System.Drawing.Point(120, 248);
+            this.statusLabel.Name = "statusLabel";
+            this.statusLabel.Size = new System.Drawing.Size(372, 29);
+            this.statusLabel.TabIndex = 7;
+            this.statusLabel.Text = "STATUS";
+            this.statusLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label4
+            // 
+            this.label4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(259, 79);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(93, 153);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "VS";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnRestart
+            // 
+            this.btnRestart.BackColor = System.Drawing.Color.Tan;
+            this.btnRestart.FlatAppearance.BorderSize = 0;
+            this.btnRestart.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnRestart.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.btnRestart.Location = new System.Drawing.Point(189, 391);
+            this.btnRestart.Name = "btnRestart";
+            this.btnRestart.Size = new System.Drawing.Size(246, 39);
+            this.btnRestart.TabIndex = 9;
+            this.btnRestart.Text = "Reset";
+            this.btnRestart.UseVisualStyleBackColor = false;
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 17F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Desktop;
+            this.ClientSize = new System.Drawing.Size(619, 442);
+            this.Controls.Add(this.btnRestart);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.statusLabel);
+            this.Controls.Add(this.computerPictureBox);
+            this.Controls.Add(this.userPictureBox);
+            this.Controls.Add(this.computerScoreLabel);
+            this.Controls.Add(this.userScoreLabel);
+            this.Controls.Add(this.btnScissors);
+            this.Controls.Add(this.btnPaper);
+            this.Controls.Add(this.btnRock);
+            this.Font = new System.Drawing.Font("Century Gothic", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.WhiteSmoke;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Main";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Rock Paper Scissors";
+            ((System.ComponentModel.ISupportInitialize)(this.userPictureBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.computerPictureBox)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRock;
+        private System.Windows.Forms.Button btnPaper;
+        private System.Windows.Forms.Button btnScissors;
+        private System.Windows.Forms.Label userScoreLabel;
+        private System.Windows.Forms.Label computerScoreLabel;
+        private System.Windows.Forms.PictureBox userPictureBox;
+        private System.Windows.Forms.PictureBox computerPictureBox;
+        private System.Windows.Forms.Label statusLabel;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnRestart;
+    }
+}
+
---
+++ Rock Paper Scissors/Forms/Main.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Rock_Paper_Scissors
+{
+    public partial class Main : Form
+    {
+        public int userScore = 0;
+        public int computerScore = 0;
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonPress(object sender, EventArgs e)
+        {
+            Button whatButton = (Button) sender;
+            userChoice(whatButton.Text);
+        }
+
+        private void userChoice(String choice)
+        {
+            String computerChoice = getComputerChoice();
+            switch (choice + computerChoice)
+            {
+                //win
+                case "RockScissors":
+                case "PaperRock":
+                case "ScissorsPaper":
+                    userWin(choice, computerChoice);
+                    break;
+                //loose
+                case "ScissorsRock":
+                case "RockPaper":
+                case "PaperScissors":
+                    userLoose(choice, computerChoice);
+                    break;
+                //draw
+                case "RockRock":
+                case "ScissorsScissors":
+                case "PaperPaper":
+                    battleDraw();
+                    break;
+            }
+        }
+
+        private void battleDraw()
+        {
+            statusLabel.Text = "draw";
+            userScoreLabel.Text = "User: " + userScore;
+            computerScoreLabel.Text = "Computer: " + computerScore;
+        }
+
+        private void userLoose(string choice, string computerChoice)
+        {
+            computerScore++;
+            userScoreLabel.Text = "User: " + userScore;
+            computerScoreLabel.Text = "Computer: " + computerScore;
+            statusLabel.Text = "You loose!, " + choice + " looses to " + computerChoice;
+        }
+
+        private void userWin(string choice, string computerChoice)
+        {
+            userScore++;
+            userScoreLabel.Text = "User: " + userScore;
+            computerScoreLabel.Text = "Computer: " + computerScore;
+            statusLabel.Text = "You win!, " + choice + " beats " + computerChoice;
+        }
+
+
+        private string getComputerChoice()
+        {
+            String[] choices = {"Rock","Paper","Scissors"};
+            Random rnd = new Random();
+            int randomInt = rnd.Next(0, 2);
+           // int randomInt = (int) Math.Floor(rnd.NextDouble() * 3.0); need true random, help lol
+            return choices[randomInt];
+
+        }
+   
+    }
+}
